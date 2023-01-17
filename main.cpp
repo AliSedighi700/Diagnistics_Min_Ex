f4451c97058d7b6daa4ca3547b95ae5f943b9e3b
Ali.S <asadighi700@gmail.com>
2023-01-17 18:45:32 +0100

    The const variables are defined

--- main.cpp
+++ main.cpp
@@ -3,11 +3,26 @@
 #include <Kokkos_Core.hpp>
 
 int main(int argc, char* argv []){
+	
+	double N_v = 10 ; // The number of nodes in V-Direction. 
+	double N_x = 5 ; // The number of nodes in X_Direction. 
+	double dv = 2 ; // The size of the cell in V_Direction. 
+	double dx = 1.25 ; // The size of the cell in X_Direction. 
+   
 
+	// <---|---|---|--- . . . ---|->V
+	//    -20 -18 -16           20
+
+
+	// <-|-----|-------|------|------|------|-> X
+  //   0   1.256   2.512  3.768  5.024  6.28 (2Pi)
+
+
+	
   Kokkos::initialize( argc, argv );
-  {
-		Kokkos::View<double******> f{};
-		std::cout << "Hello World\n";
+  { 
+
+
   }
   Kokkos::finalize();
 
