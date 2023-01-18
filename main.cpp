38a0e681429f9ca5710e247e34ac0d15470a8c91
Ali.S <asadighi700@gmail.com>
2023-01-18 08:44:30 +0100

    First try to define f(x,v)

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char* argv []){
 	int N_v = 10 ; // The number of nodes in V-Direction. 
 	double N_x = 5 ; // The number of nodes in X_Direction. 
 	int  dv = 2 ; // The size of the cell in V_Direction. 
-	double dx = 1.25 ; // The size of the cell in X_Direction. 
-   
+	double dx = 1.256 ; // The size of the cell in X_Direction. 
+  const float e = 2.718228183 ; 
 
 	// <---|---|---|--- . . . ---|->V
 	//    -20 -18 -16           20
@@ -26,36 +26,50 @@ int main(int argc, char* argv []){
 	
   Kokkos::initialize( argc, argv );
   {
-		int V_max = -20; // maximum value of velocity. 
-   	double X_max = 2 *  M_PI ; //maximum value of position. 
+		int V_max = 20; // maximum value of velocity. 
+   	double X_max = 6.28 ; //maximum value of position. 
 
 
-		std::vector<int> Velocities ; 
-		std::vector<double> Positions ; 
+		std::vector<int> v_1 ; 
+		std::vector<int> v_2 ; 
+		std::vector<int> v_3 ; 
+		
+		std::vector<double> p_1 ;
+   	std::vector<double> p_2 ;
+    std::vector<double> p_3 ;
+   
 
-		double x = 0 ; 
-		int v = 0 ; 
 
-		for(double i = 1 ; i < X_max ; i++) // definition of x_coordinate. 
-		{
+		double x = 0.0 ; 
+		int v = -20 ; 
+
+		for(double i = 0 ; i < X_max ; i++) // definition of x_coordinate. 
+		{ 
 			x += dx ;
-			Positions.push_back(x) ; 
+			p_1.push_back(x) ;
+
+			if(x >= X_max)
+			break ; 
 		}
 		
-    for(double number : Positions ) 
+    for(double number : p_1 )    // check whether the elements are correct. 
 			std::cout << number << std::endl ;
 
-		for(int i = 0  ; i < V_max ; i++)
+    
+		v_1.push_back(v) ; 
+
+		for(int i = 0  ; i < V_max  ; i++) // Definition of v-coordinate.
 		{
 			v += dv ; 
-			Velocities.push_back(v) ; 
+			v_1.push_back(v) ; 
 		}
 
+   for(int number : v_1) // check the v-elements are correct. 
+	   std::cout << number << std::endl ; 
 
-    for(double number : Velocities ) 
-			std::cout << number << std::endl ;
+   Kokkos::View<double ******> f{} ; //Distribution_Function definition (6D View).
 
-
+   float M_Dist = ( 1 / sqrt(pow( 1 / (2 * M_PI),   3)) )  * 4 * M_PI * pow(v,2) * pow(e, 1 );
 
   }
   Kokkos::finalize();
