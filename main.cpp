78f754cea58ed7bb78908aa16ff1b3ae12b4874f
Ali.S <asadighi700@gmail.com>
2023-01-19 11:01:39 +0100

    The spaces issue is done (2)

--- main.cpp
+++ main.cpp
@@ -25,46 +25,43 @@ int main(int argc, char* argv []){
 	
   Kokkos::initialize( argc, argv );
   {
-		int V_max = 20; // maximum value of velocity. 
+    int V_max = 20; // maximum value of velocity. 
    	double X_max = 6.28 ; //maximum value of position. 
     int dv = V_max / N_v ; 
 
-		std::vector<int> v_1 ; 
-		std::vector<int> v_2 ; 
-		std::vector<int> v_3 ; 
+    std::vector<int> v_1 ; 
+    std::vector<int> v_2 ; 
+    std::vector<int> v_3 ; 
 		
-		std::vector<double> p_1 ;
+    std::vector<double> p_1 ;
    	std::vector<double> p_2 ;
     std::vector<double> p_3 ;
-   
 
+    double x = 0 ; 
+    for(double i = 0 ; i < X_max ; i++)  // X-1 definition. 
+    { 
+      x += dx ;
+      p_1.push_back(x) ;
 
-		double x = 0 ; 
-		for(double i = 0 ; i < X_max ; i++)  // X-1 definition. 
-		{ 
-			x += dx ;
-			p_1.push_back(x) ;
+      if(x >= X_max)
+      break ; 
+      }
 
-			if(x >= X_max)
-			break ; 
-		}
+    double x_2 = 0 ; 
+    for(double i = 0 ; i < X_max ; i++) // X-2 definition. 
+    { 
+      x_2 += dx ;
+      p_2.push_back(x_2) ;
 
-
-		double x_2 = 0 ; 
-  	for(double i = 0 ; i < X_max ; i++) // X-2 definition. 
-		{ 
-			x_2 += dx ;
-			p_2.push_back(x_2) ;
-
-			if(x_2 >= X_max)
-			break ; 
+      if(x_2 >= X_max)
+      break ;  
 		}
     
 
-		double x_3 = 0 ; 		
-		for(double i = 0 ; i < X_max ; i++)  // X-3 definition. 
-		{ 
-		  x_3 += dx ;
+    double x_3 = 0 ; 		
+    for(double i = 0 ; i < X_max ; i++)  // X-3 definition. 
+    { 
+      x_3 += dx ; 
 			p_3.push_back(x_3) ;
 
 			if(x_3 >= X_max)
@@ -72,32 +69,31 @@ int main(int argc, char* argv []){
 		}
 
 
-
-		int V_1 = -20 ; 
-		v_1.push_back(V_1) ; 
+    int V_1 = -20 ; 
+    v_1.push_back(V_1) ; 
 		for(int i = 0  ; i < V_max  ; i++) // Definition of v1-coordinate.
 		{
-		 	V_1 += dv ; 
-			v_1.push_back(V_1) ; 
+		  V_1 += dv ; 
+			v_1.push_back(V_1) ;
 		}
 
 
-		int V_2 = -20 ; 		
+    int V_2 = -20 ;
 		v_2.push_back(V_2) ; 
 		for(int i = 0  ; i < V_max  ; i++) // Definition of v2-coordinate.
 		{
-			V_2 += dv ; 
-			v_2.push_back(V_2) ; 
+		  V_2 += dv ; 
+			v_2.push_back(V_2) ;
 		}
 
 
-		int V_3 = -20 ; 
+    int V_3 = -20 ; 
 		v_3.push_back(V_3) ; 
 		for(int i = 0  ; i < V_max  ; i++) // Definition of v3-coordinate.
 		{
-			V_3 += dv ; 
+		  V_3 += dv ; 
 			v_3.push_back(V_3) ; 
-		}
+			}
 
 
 
@@ -107,15 +103,13 @@ int main(int argc, char* argv []){
 
 
 // feed the 6D View with values if the distribution function .
-		Kokkos::parallel_for(
+    Kokkos::parallel_for(
 		        "rho",
-						  Kokkos::MDRangePolicy<Kokkos::Rank<6>>(
-							  {0,0,0,0,0,0}, {N_v, N_v, N_v, N_x, N_x, N_x}),
-								  KOKKOS_LAMBDA(size_t i, size_t j, size_t k, size_t l, size_t m, size_t n){
-
-    	f(i,j,k,l,m,n) = M_Dist * pow( e , -0.5 * pow( v_1[i], 2 )) * pow( e , -0.5 * pow( v_2[j] , 2 )) * pow( e, -0.5 * pow( v_3[k] , 2 )) ;
-
-             }); 
+						Kokkos::MDRangePolicy<Kokkos::Rank<6>>(
+						  {0,0,0,0,0,0}, {N_v, N_v, N_v, N_x, N_x, N_x}),
+						    KOKKOS_LAMBDA(size_t i, size_t j, size_t k, size_t l, size_t m, size_t n){ 
+								  f(i,j,k,l,m,n) = M_Dist * pow( e , -0.5 * pow( v_1[i], 2 )) * pow( e , -0.5 * pow( v_2[j] , 2 )) * pow( e, -0.5 * pow( v_3[k] , 2 )) ;
+              }); 
 
 // Integration. due to race condition, we do the triple integral over velocity space with 3 parallel and 3 serial loop using Kokko parallel_for. 
 
