a3cb35e7ce14d6051fae650b39bd5c4b6844e40f
Ali.S <asadighi700@gmail.com>
2023-01-20 11:50:00 +0100

    rho-E finished version

--- main.cpp
+++ main.cpp
@@ -7,113 +7,77 @@
 
 int main(int argc, char* argv []){
 
-// in order to define the coordinates, we should define the number of nodes in the grid and the size of each cell. 
-
-	size_t N_v = 10 ; // The number of nodes in V-Direction. 
-	size_t  N_x = 5 ; // The number of nodes in X_Direction. 
+	size_t N_v = 11 ; // The number of nodes in V-Direction. 
+	size_t  N_x = 10 ; // The number of nodes in X_Direction. 
 	double dx = (2.0/N_x) * M_PI ; // The size of the cell in X_Direction. 
-  const float e = 2.718228183 ; 
-
-	// <---|---|---|--- . . . ---|->V
-	//    -20 -18 -16           20
+  const float e = 2.71828183 ; 
 
 
-	// <-|-----|-------|------|------|------|-> X
-  //   0   1.256   2.512  3.768  5.024  6.28 (2Pi)
-
-
-	
   Kokkos::initialize( argc, argv );
   {
-    int V_max = 20; // maximum value of velocity. 
    	double X_max = 6.28 ; //maximum value of position. 
-    int dv = V_max / N_v ; 
+		double V_max = 4 ; 
+    double dv = 2*V_max / (N_v - 1) ; 
 
-    std::vector<int> v_1 ; 
-    std::vector<int> v_2 ; 
-    std::vector<int> v_3 ; 
+    std::vector<double> v_1 ; 
+    std::vector<double> v_2 ; 
+    std::vector<double> v_3 ; 
 		
     std::vector<double> p_1 ;
    	std::vector<double> p_2 ;
     std::vector<double> p_3 ;
 
-    double x = 0 ; 
-    for(double i = 0 ; i < X_max ; i++)  // X-1 definition. 
-    { 
-      x += dx ;
-      p_1.push_back(x) ;
 
-      if(x >= X_max)
-      break ; 
-      }
+    for(double i = 0 ; i <  N_x ; i++)  // X-1 definition. 
+      p_1.push_back(i * dx) ;
 
-    double x_2 = 0 ; 
-    for(double i = 0 ; i < X_max ; i++) // X-2 definition. 
-    { 
-      x_2 += dx ;
-      p_2.push_back(x_2) ;
 
-      if(x_2 >= X_max)
-      break ;  
-		}
+
+    for(double i = 0 ; i <  N_x ; i++)  // X-2 definition. 
+      p_2.push_back(i * dx) ;
+	 
+      
+    for(double i = 0 ; i < N_x ; i++)  // X-3 definition. 
+			p_3.push_back(i * dx) ;
+
+
+		for(double i = 0  ; i < N_v  ; i++) // Definition of v1-coordinate.
+			v_1.push_back(-1 * V_max + i *dv) ;
+      
+		for(double i = 0  ; i < N_v  ; i++) // Definition of v2-coordinate.
+			v_2.push_back(-1 * V_max + i * dv) ;
+
+
+		for(double i = 0  ; i < N_v  ; i++) // Definition of v3-coordinate.
+			v_3.push_back(-1 * V_max + i * dv) ; 
+
+//    for(double i = 0 ; i < N_x ; i++)
+	//	{
+    //   std::cout << p_1[i] << " " <<  p_2[i] << " " << p_3[i] << "\n" ; 
+	//	}
+
+    //for(double i = 0 ; i < N_v ; i++)
+	 	//{
+       //std::cout << v_1[i] << " " <<  v_2[i] << " " << v_3[i] << "\n" ; 
+		//}
+
+
+
+   	Kokkos::View<double ******> f{"Distribution", N_x, N_x, N_x, N_v, N_v, N_v} ; //Distribution_Function definition (6D View).
     
-
-    double x_3 = 0 ; 		
-    for(double i = 0 ; i < X_max ; i++)  // X-3 definition. 
-    { 
-      x_3 += dx ; 
-			p_3.push_back(x_3) ;
-
-			if(x_3 >= X_max)
-			break ; 
-		}
+    float M_Dist = ( sqrt(pow( 1 / (2 * M_PI),   3)) );
 
 
-    int V_1 = -20 ; 
-    v_1.push_back(V_1) ; 
-		for(int i = 0  ; i < V_max  ; i++) // Definition of v1-coordinate.
-		{
-		  V_1 += dv ; 
-			v_1.push_back(V_1) ;
-		}
-
-
-    int V_2 = -20 ;
-		v_2.push_back(V_2) ; 
-		for(int i = 0  ; i < V_max  ; i++) // Definition of v2-coordinate.
-		{
-		  V_2 += dv ; 
-			v_2.push_back(V_2) ;
-		}
-
-
-    int V_3 = -20 ; 
-		v_3.push_back(V_3) ; 
-		for(int i = 0  ; i < V_max  ; i++) // Definition of v3-coordinate.
-		{
-		  V_3 += dv ; 
-			v_3.push_back(V_3) ; 
-			}
-
-
-
-   	Kokkos::View<double ******> f{"Distribution", N_v, N_v, N_v, N_x, N_x, N_x } ; //Distribution_Function definition (6D View).
-    
-    float M_Dist = ( 1 / sqrt(pow( 1 / (2 * M_PI),   3)) );
-
-
-// feed the 6D View with values if the distribution function .
+    ////////calculating the particle density///////////
     Kokkos::parallel_for(
 		        "rho",
 						Kokkos::MDRangePolicy<Kokkos::Rank<6>>(
-						  {0,0,0,0,0,0}, {N_v, N_v, N_v, N_x, N_x, N_x}),
+						  {0,0,0,0,0,0}, {N_x, N_x, N_x, N_v, N_v, N_v}),
 						    KOKKOS_LAMBDA(size_t i, size_t j, size_t k, size_t l, size_t m, size_t n){ 
-								  f(i,j,k,l,m,n) = M_Dist * pow( e , -0.5 * pow( v_1[i], 2 )) * pow( e , -0.5 * pow( v_2[j] , 2 )) * pow( e, -0.5 * pow( v_3[k] , 2 )) ;
+								  f(i,j,k,l,m,n) = M_Dist * pow( e , -0.5 * pow( v_1[l], 2 )) * pow( e , -0.5 * pow( v_2[m] , 2 )) * pow( e, -0.5 * pow( v_3[n] , 2 )) ;
               }); 
 
-// Integration. due to race condition, we do the triple integral over velocity space with 3 parallel and 3 serial loop using Kokko parallel_for. 
-
-    Kokkos::View<double ***> Sum {"label", N_x, N_x, N_x} ; 
+		Kokkos::View<double ***> Sum_rho ("for rho", N_x, N_x, N_x) ;					
 
 		Kokkos::parallel_for(
 		        "Sumd3v",
@@ -124,14 +88,48 @@ int main(int argc, char* argv []){
 					for(size_t i3 = 0 ; i3 < v_1.size() ; i3++)
 				  	for(size_t i4 = 0 ; i4 < v_2.size() ; i4++)
 					    for(size_t i5 = 0 ; i5 < v_3.size() ; i5++ )
-							  Sum(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) ; 
-          Sum(i0, i1, i2) *= pow(dv, 3)  ; 
+							  Sum_rho(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) ; 
+          Sum_rho(i0, i1, i2) *= (dv * dv * dv) ;      
+				 });
+
+
+
+				 ////calculating the energy////////
+
+    // feed the 6D View with values if the distribution function .
+    Kokkos::parallel_for(
+		        "rho",
+						Kokkos::MDRangePolicy<Kokkos::Rank<6>>(
+						  {0,0,0,0,0,0}, {N_x, N_x, N_x, N_v, N_v, N_v}),
+						    KOKKOS_LAMBDA(size_t i, size_t j, size_t k, size_t l, size_t m, size_t n){ 
+								  f(i,j,k,l,m,n) = M_Dist * pow( e , -0.5 * pow( v_1[l], 2 )) * pow( e , -0.5 * pow( v_2[m] , 2 )) * pow( e, -0.5 * pow( v_3[n] , 2 )) ;
+              }); 
+
+    // Integration. due to race condition, we do the triple integral over velocity space with 3 parallel and 3 serial loop using Kokko parallel_for. 
+
+    Kokkos::View<double ***> Sum_E {"Energy", N_x, N_x, N_x} ; 
+
+		Kokkos::parallel_for(
+		        "Sumd3v",
+						  Kokkos::MDRangePolicy<Kokkos::Rank<3>>(
+                {0,0,0},{N_x, N_x, N_x}),
+							    KOKKOS_LAMBDA(size_t i0, size_t i1, size_t i2){
+					
+					for(size_t i3 = 0 ; i3 < v_1.size() ; i3++)
+				  	for(size_t i4 = 0 ; i4 < v_2.size() ; i4++)
+					    for(size_t i5 = 0 ; i5 < v_3.size() ; i5++ )
+							  Sum_E(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) * ((v_1[i3] * v_1[i3]) + (v_2[i4] * v_2[i4]) + (v_3[i5] * v_3[i5])) ;  
+          Sum_E(i0, i1, i2) *= (dv * dv * dv)  ; 
+					
          
 				 });
+
+				 std::cout << " The particle number density: " << Sum_rho(0,0,0) <<  "\n" ;  
+				 std::cout << " The Energy: " << Sum_E(0,0,0)  << "\n" ; 
   }
   Kokkos::finalize();
 
-	return 0; 
+	return 0 ; 
 
   
 }
