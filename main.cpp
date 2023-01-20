1e3d8c2c36bd502c63541ce7bee56a426bfeaebf
Ali.S <asadighi700@gmail.com>
2023-01-20 17:52:31 +0100

    Flow 90% done

--- main.cpp
+++ main.cpp
@@ -32,7 +32,6 @@ int main(int argc, char* argv []){
       p_1.push_back(i * dx) ;
 
 
-
     for(double i = 0 ; i <  N_x ; i++)  // X-2 definition. 
       p_2.push_back(i * dx) ;
 	 
@@ -43,7 +42,9 @@ int main(int argc, char* argv []){
 
 		for(double i = 0  ; i < N_v  ; i++) // Definition of v1-coordinate.
 			v_1.push_back(-1 * V_max + i *dv) ;
-      
+
+ 
+	  
 		for(double i = 0  ; i < N_v  ; i++) // Definition of v2-coordinate.
 			v_2.push_back(-1 * V_max + i * dv) ;
 
@@ -51,50 +52,16 @@ int main(int argc, char* argv []){
 		for(double i = 0  ; i < N_v  ; i++) // Definition of v3-coordinate.
 			v_3.push_back(-1 * V_max + i * dv) ; 
 
-//    for(double i = 0 ; i < N_x ; i++)
-	//	{
-    //   std::cout << p_1[i] << " " <<  p_2[i] << " " << p_3[i] << "\n" ; 
-	//	}
 
-    //for(double i = 0 ; i < N_v ; i++)
-	 	//{
-       //std::cout << v_1[i] << " " <<  v_2[i] << " " << v_3[i] << "\n" ; 
-		//}
 
+
+  	std::array< std::vector<double>, 3> V{v_1,v_2,v_3}; 
+    
 
 
    	Kokkos::View<double ******> f{"Distribution", N_x, N_x, N_x, N_v, N_v, N_v} ; //Distribution_Function definition (6D View).
     
     float M_Dist = ( sqrt(pow( 1 / (2 * M_PI),   3)) );
-
-
-    ////////calculating the particle density///////////
-    Kokkos::parallel_for(
-		        "rho",
-						Kokkos::MDRangePolicy<Kokkos::Rank<6>>(
-						  {0,0,0,0,0,0}, {N_x, N_x, N_x, N_v, N_v, N_v}),
-						    KOKKOS_LAMBDA(size_t i, size_t j, size_t k, size_t l, size_t m, size_t n){ 
-								  f(i,j,k,l,m,n) = M_Dist * pow( e , -0.5 * pow( v_1[l], 2 )) * pow( e , -0.5 * pow( v_2[m] , 2 )) * pow( e, -0.5 * pow( v_3[n] , 2 )) ;
-              }); 
-
-		Kokkos::View<double ***> Sum_rho ("for rho", N_x, N_x, N_x) ;					
-
-		Kokkos::parallel_for(
-		        "Sumd3v",
-						  Kokkos::MDRangePolicy<Kokkos::Rank<3>>(
-                {0,0,0},{N_x, N_x, N_x}),
-							    KOKKOS_LAMBDA(size_t i0, size_t i1, size_t i2){
-					
-					for(size_t i3 = 0 ; i3 < v_1.size() ; i3++)
-				  	for(size_t i4 = 0 ; i4 < v_2.size() ; i4++)
-					    for(size_t i5 = 0 ; i5 < v_3.size() ; i5++ )
-							  Sum_rho(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) ; 
-          Sum_rho(i0, i1, i2) *= (dv * dv * dv) ;      
-				 });
-
-
-
-				 ////calculating the energy////////
 
     // feed the 6D View with values if the distribution function .
     Kokkos::parallel_for(
@@ -107,7 +74,18 @@ int main(int argc, char* argv []){
 
     // Integration. due to race condition, we do the triple integral over velocity space with 3 parallel and 3 serial loop using Kokko parallel_for. 
 
-    Kokkos::View<double ***> Sum_E {"Energy", N_x, N_x, N_x} ; 
+    Kokkos::View<double ***> Sum_E ("Energy", N_x, N_x, N_x) ; 
+		Kokkos::View<double ***> Sum_rho ("rho", N_x, N_x, N_x) ;					
+		Kokkos::View<double ***> Sum_u ("Flow", N_x, N_x, N_x) ; 
+    
+
+
+	
+		std::array< Kokkos::View<double ***>,3> U{Kokkos::View<double ***>{"u1", N_x, N_x, N_x}, 
+																							Kokkos::View<double ***>{"u2", N_x, N_x, N_x},
+																							Kokkos::View<double ***>{"u3", N_x, N_x, N_x}} ; 
+    
+
 
 		Kokkos::parallel_for(
 		        "Sumd3v",
@@ -118,14 +96,31 @@ int main(int argc, char* argv []){
 					for(size_t i3 = 0 ; i3 < v_1.size() ; i3++)
 				  	for(size_t i4 = 0 ; i4 < v_2.size() ; i4++)
 					    for(size_t i5 = 0 ; i5 < v_3.size() ; i5++ )
-							  Sum_E(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) * ((v_1[i3] * v_1[i3]) + (v_2[i4] * v_2[i4]) + (v_3[i5] * v_3[i5])) ;  
+							{
+								Sum_rho(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) ; 
+							  Sum_E(i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) * ((v_1[i3] * v_1[i3]) + (v_2[i4] * v_2[i4]) + (v_3[i5] * v_3[i5]))  ;  
+                for(int i = 0 ; i < 3 ; i++)
+								(
+								  U[i](i0, i1, i2) += f(i0, i1, i2, i3, i4, i5) * V[i][i3]  ; //what about i1,i2,i3? we need to iterate over them? 
+	              )
+
+							}
+
+					Sum_rho(i0, i1, i2) *= (dv * dv * dv) ; 			
           Sum_E(i0, i1, i2) *= (dv * dv * dv)  ; 
+					U[0](i0, i1, i2) *= (dv * dv * dv) ; 
+					U[1](i0, i1, i2) *= (dv * dv * dv) ; 
+					U[2](i0, i1, i2) *= (dv * dv * dv) ; 
 					
          
 				 });
 
 				 std::cout << " The particle number density: " << Sum_rho(0,0,0) <<  "\n" ;  
 				 std::cout << " The Energy: " << Sum_E(0,0,0)  << "\n" ; 
+				 for(int i = 0 ; i < 3 ; i++)
+         {
+				   std::cout << " The flow:  " << U[i](0,0,0) << "\n" ; 
+				 }
   }
   Kokkos::finalize();
 
